a182b87b7d856be1baef972aecc7bc4de01ab1b9
Franklin Junior <franklinjunior12@hotmail.com>
2024-03-24 15:49:41 -0300

    Add npc health bar and damage on projectile

--- scripts/basic_projectile.gd
+++ scripts/basic_projectile.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var VELOCITY_MAG = 200
 var velocity = Vector2.ZERO
 
+const damage = 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,5 +19,7 @@ func _on_body_entered(body):
 	if body is StaticBody2D:
 		print("collided with floor or wall")
 	elif body is CharacterBody2D:
-		print("collided with character")
+		var health_bar = body.get_node_or_null("HealthBar")
+		if health_bar:
+			health_bar.value -= damage
 	queue_free()
