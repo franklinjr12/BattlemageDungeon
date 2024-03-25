d9ba8bb38dad11dc3657f4a1c865e2d4652294a3
Franklin Junior <franklinjunior12@hotmail.com>
2024-03-25 11:46:33 -0300

    Enemy detect player

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
-
 const drop_experience = 70
+
+enum NpcState {IDLE, CHASING}
+
+var current_state = NpcState.IDLE
+var player_reference = null
+var direction_chasing = Vector2.ZERO
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,7 +17,15 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _process(delta):
 	if $HealthBar.value <= 0:
 		died()
-		
+	if current_state == NpcState.CHASING:
+		var running_scene = get_parent()
+		if running_scene.name == "world":
+			if player_reference == null:
+				player_reference = running_scene.get_node("Player")
+			direction_chasing = player_reference.position - position
+			velocity.x = direction_chasing.normalized().x * SPEED
+
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -28,3 +41,11 @@ func give_exp():
 
 func suffer_damage(damage):
 	$HealthBar.value -= damage
+
+
+func _on_player_detection_area_body_entered(body):
+	# since it should detect only the player as it has its on layer no need to cast ?
+	if body is CharacterBody2D:
+		current_state = NpcState.CHASING
+		print("chasing player now")
+
