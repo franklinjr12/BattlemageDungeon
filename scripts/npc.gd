752adf57b66d23d850e179047de6abf66fc94b4f
Franklin Junior <franklinjunior12@hotmail.com>
2024-03-28 10:24:24 -0300

    Add 2 more enemies

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -4,11 +4,11 @@ extends CharacterBody2D
 @onready var SPEED = $CharacterAttributes.speed
 const JUMP_VELOCITY = -400.0
 @onready var drop_experience = $CharacterAttributes.drop_experience
-const ATTACK_RANGE = 40
 
 enum NpcState {IDLE, CHASING, ATTACKING}
 
 var current_state = NpcState.IDLE
+var attack_range = 40
 var player_reference = null
 var direction_chasing = Vector2.ZERO
 var attack_cooldown = 0.5
@@ -21,6 +21,8 @@ var attack_on_cooldown = false
 func _ready():
 	$AttackCooldown.wait_time = attack_cooldown
 	$HealthBar.max_value = $CharacterAttributes.health
+	$HealthBar.value = $HealthBar.max_value
+	attack_range = $Sprite2D.texture.get_width()
 
 func _process(delta):
 	if $HealthBar.value <= 0:
@@ -47,7 +49,6 @@ func give_exp():
 func suffer_damage(damage):
 	$HealthBar.value -= damage
 
-
 func _on_player_detection_area_body_entered(body):
 	# since it should detect only the player as it has its on layer no need to cast ?
 	if body is CharacterBody2D:
@@ -70,12 +71,12 @@ func handle_chasing(delta):
 		else:
 			$Sprite2D.flip_h = false
 		velocity.x = direction_chasing.normalized().x * SPEED
-		if (player_reference.position - position).length() < ATTACK_RANGE:
+		if (player_reference.position - position).length() < attack_range:
 			velocity.x -= direction_chasing.normalized().x * SPEED
 			current_state = NpcState.ATTACKING
 
 func handle_attacking(delta):
-	if (player_reference.position - position).length() > ATTACK_RANGE:
+	if (player_reference.position - position).length() > attack_range:
 		current_state = NpcState.CHASING
 	else:
 		if can_attack and !attack_on_cooldown:
@@ -84,13 +85,18 @@ func handle_attacking(delta):
 			var running_scene = get_parent()
 			var new_attack = attack.instantiate()
 			new_attack.damage = $CharacterAttributes.damage
-			var npc_shape_offset = $CollisionShape2D.shape.radius
+			var shape = $CollisionShape2D.shape
+			var npc_shape_offset = 0
+			if shape is CapsuleShape2D:
+				npc_shape_offset = shape.radius
+			elif shape is RectangleShape2D:
+				npc_shape_offset = shape.size.x
 			var offset_x = 0
 			var attack_shape_offset = new_attack.get_node("CollisionShape2D").shape.radius
 			if player_reference.position.x > position.x:
-				offset_x += attack_shape_offset * 1.5 + attack_shape_offset
+				offset_x += attack_shape_offset * 1.5 + 1.5 * npc_shape_offset
 			else:
-				offset_x += -attack_shape_offset * 1.5 - attack_shape_offset
+				offset_x += -attack_shape_offset * 1.5 - 1.5 * npc_shape_offset
 				new_attack.get_node("Sprite2D").flip_h = true
 			var v = position
 			v.x += offset_x
